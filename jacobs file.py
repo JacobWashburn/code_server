7dc7dd8c4be65eaaee1b66158a0f773fc1de4bec
Jacob Washburn <jacobrwashburn@gmail.com>
2019-02-16 16:51:14 -0700

    inputs added

--- jacobs file.py
+++ jacobs file.py
@@ -1,1 +1,12 @@
 print("hellos")
+age = input("please enter how old you are: " )
+print(age)
+name = input("please enter your name: ")
+print(name)
+#git push before connecting to server
+#to put your code on the server
+# command is "ssh root@donebynathan.com"
+# password is "nathanandjacob"
+#then "cd /public/www/pythonstuff/code_server/ "
+#now you can run git pull
+#to exit from the server type "exit"
